88842c36f280eea37fd00ce83b632faf9e052e11
Alexander Scaini <alexanderscaini@gmail.com>
2020-10-10 21:09:02 +0000

    removed a console.log

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
   $('#tweet-text').on('input', function() {
     let numberOfChars = $(this).serialize().slice(5).length;
-    console.log(numberOfChars)
     $('.counter').text(140 - numberOfChars) 
     if ((140 - numberOfChars) > 0) {
       $('.counter').removeClass('red')
